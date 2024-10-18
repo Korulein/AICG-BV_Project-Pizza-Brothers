bb52a258bbb549765c0e3eed52c7a8321373543b
DemonicScreeching <98325345+DemonicScreeching@users.noreply.github.com>
2024-10-18 14:18:22 +0200

    Add files via upload
    - Edited Ihsans mario.py to be able to work continuosly untill order is empty. 
    - Turned the functions into seperate def functions.
    - Added the function to append the orders into a seperate json file (json file needs a list to work properly, so [ ]).

--- Prototype App Cache/mario.py
+++ Prototype App Cache/mario.py
@@ -7,26 +7,45 @@ order = []
 appended_order = []
 orders = {}
 order_number = 101
+def enter_order():
+    global order, orders
+    while True:
+     pizza_type = input("Enter Pizza Type: (Enter to Quit)")
+     if pizza_type == '':
+        return orders
+
+     pizza_quantity = int(input("Enter Pizza Quantity: "))
+    
+     order = (pizza_type , pizza_quantity)
+     appended_order.append(order)
+        
+    
+    
+     orders = { "order number": order_number,
+               "order": appended_order }
+
+def send_luigi():
+ global orders, appended_order
+ with open("orders.json", "r") as file:
+    try:
+     orderlist=json.load(file)
+    except json.JSONDecodeError:
+        orderlist = []  # Initialize an empty list if file is empty or invalid
+ orderlist.append(orders)
+ out_file = open("orders.json","w")
+ json.dump(orderlist, out_file, indent=4)
+ orders.clear()
+ appended_order.clear()
+
+ return True
 
 while True:
-    pizza_type = input("Enter Pizza Type: (Enter to Quit)")
-    if pizza_type == '':
-        
-        break
-
-    pizza_quantity = int(input("Enter Pizza Quantity: "))
-    
-    order = (pizza_type , pizza_quantity)
-    appended_order.append(order)
-        
-    
-    
-    orders = { "order number": order_number,
-               "order": appended_order }
-   
-out_file = open("sample.json")
-json.dump(orders, out_file, indent=4)
-out_file.close()
-print(orders)
+   orders=enter_order()
+   order_number+=1
+   if orders== {}:
+      break 
+   else :
+      send_luigi()
+      print(orders)
 
 
